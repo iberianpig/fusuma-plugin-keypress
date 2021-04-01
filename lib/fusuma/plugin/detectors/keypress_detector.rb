b0e483cab2102281795868b9eac5e60155f273ae
iberianpig <yhkyky@gmail.com>
2021-04-01 22:56:35 +0900

    fix(detector): Always watch buffers for detect as context

--- lib/fusuma/plugin/detectors/keypress_detector.rb
+++ lib/fusuma/plugin/detectors/keypress_detector.rb
@@ -5,7 +5,13 @@ module Fusuma
     module Detectors
       # Detect KeypressEvent from KeypressBuffer
       class KeypressDetector < Detector
+        SOURCES = ['keypress'].freeze
         BUFFER_TYPE = 'keypress'
+
+        # Always watch buffers and detect them.
+        def watch?
+          true
+        end
 
         # @param buffers [Array<Event>]
         # @return [Event] if event is detected
@@ -15,9 +21,7 @@ module Fusuma
 
           return if buffer.empty?
 
-          records = buffer.events.select do |e|
-            e.record.status == 'pressed'
-          end.map(&:record)
+          records = buffer.events.select { |e| e.record.status == 'pressed' }.map(&:record)
 
           index_record = Events::Records::IndexRecord.new(
             index: create_index(records: records),
