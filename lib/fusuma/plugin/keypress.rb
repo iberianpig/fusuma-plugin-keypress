edcef4224efa68b84e1c103b75bab0b44c82927b
iberianpig <yhkyky@gmail.com>
2024-01-20 09:25:04 +0900

    chore: standardrb --fix

--- lib/fusuma/plugin/keypress.rb
+++ lib/fusuma/plugin/keypress.rb
@@ -2,9 +2,9 @@
 
 # fusuma will `require fusuma-plugin-xxxx/plugin/xxxx.rb`
 
-require_relative "./keypress/version"
-require_relative "./events/records/keypress_record"
-require_relative "./filters/keypress_filter"
-require_relative "./parsers/keypress_parser"
-require_relative "./buffers/keypress_buffer"
-require_relative "./detectors/keypress_detector"
+require_relative "keypress/version"
+require_relative "events/records/keypress_record"
+require_relative "filters/keypress_filter"
+require_relative "parsers/keypress_parser"
+require_relative "buffers/keypress_buffer"
+require_relative "detectors/keypress_detector"
